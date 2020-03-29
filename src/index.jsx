a5ca8a810cbadf83becbd0466badf2324e65b24d
morellaj <alexjamesmorelli@gmail.com>
2020-03-29 15:28:53 -0400

    added normalize.css and global style

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,35 @@ import 'typeface-roboto';
 import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 // File dependencies
 const HomePage = lazy(() => import('./components/home/HomePage'));
 const ProjectsPage = lazy(() => import('./components/projects/ProjectsPage'));
 const ArticlesPage = lazy(() => import('./components/articles/ArticlesPage'));
 
+const GlobalStyle = createGlobalStyle`
+ ${normalize}
+ body {
+   margin: 0;
+   font-family: 'Roboto', sans-serif;
+    min-height: 110vh;
+    position: relative;
+ }
+
+ h1 {
+   margin: 0;
+ }
+
+ p {
+   margin: 0;
+ }
+`;
 
 ReactDOM.render((
   <BrowserRouter>
+    <GlobalStyle />
     <Navbar />
     <Suspense fallback={<div />}>
       <Switch>
